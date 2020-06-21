77db96c50027fa563a27e76659b79e7619667d08
azharshahstl <azharshahstl@yahoo.com>
2020-06-21 12:40:10 -0500

    Added googlemaps with marker at st.louis

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log('Dom loaded')
-    initMap();
 
-    function initMap() {
-        var uluru = {lat: -25.344, lng: 131.036};
-        var map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: uluru});
-        var marker = new google.maps.Marker({position: uluru, map: map});
-    }
+var script = document.createElement('script');
+script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyA99nF4WLsPeygHrRoJOTRH1Bk5DBJjoyg&callback=initMap';
+script.defer = true;
+script.async = true;
 
-})
+const stl = { lat: 38.6270, lng: -90.1994 }
+var map;
+
+
+window.initMap = function() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: stl,
+        zoom: 8
+      });
+      const marker = new google.maps.Marker({position: stl, map: map});
+  
+};
+
+
+document.head.appendChild(script);
+         
+
+   
